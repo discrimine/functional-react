dab05c8d3a78e358aa13093a6192d5af2794e976
discrimine <discrimine96@gmail.com>
2020-04-15 23:11:24 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/main/Header';
 import Main from './components/main/Main';
 import Rooms from './components/rooms/Rooms';
 import Events from './components/events/Events';
+import Login from './components/users/Login';
+import Signup from './components/users/Signup';
 
 import './App.css';
 
@@ -18,6 +20,8 @@ function App() {
           <Route path="/home" component={Main} />
           <Route path="/rooms" component={Rooms} />
           <Route path="/events" component={Events} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
           <Redirect from='/' to='/home'/>
         </Switch>
       </BrowserRouter>
--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router";
 
 export default function Header() {
+
+  const currentLocation = useLocation().pathname || '/';
+  
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/rooms">Rooms</Link>
-        </li>
-        <li>
-          <Link to="/events">Events</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="nav-link" to="/">
+        <img src="/logo192.png" width="50" height="50" className="d-inline-block align-top" alt=""></img>
+      </Link>
+      <div className="collapse navbar-collapse justify-content-between">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className={"nav-link " + (currentLocation === '/rooms' ? 'active' : '' )}
+              to="/rooms">Rooms
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className={"nav-link " + (currentLocation === '/events' ? 'active' : '' )}
+              to="/events">Events
+            </Link>
+          </li>
+        </ul>
+        
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className={"nav-link " + (currentLocation === '/login' ? 'active' : '' )}
+              to="/login">Log In
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className={"nav-link border border-success rounded-pill " + (currentLocation === '/signup' ? 'active' : '' )}
+              to="/signup">Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
