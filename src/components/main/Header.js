96ce7bc3199a904d0d9a3bca0d59bf18a5cb6591
discrimine <discrimine96@gmail.com>
2020-04-15 23:14:13 +0300

    quotes corrected

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useLocation } from "react-router";
+import { useLocation } from 'react-router';
 
 export default function Header() {
 
