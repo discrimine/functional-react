85bfa03bf057ed78b8b62f1de05fd534d4d2c1c0
discrimine <discrimine96@gmail.com>
2020-04-16 11:16:22 +0300

    removed excessive spaces

--- src/components/main/HeaderLink.js
+++ src/components/main/HeaderLink.js
@@ -6,13 +6,11 @@ export default function HeaderLink(props) {
   const title = props.title || props.link[0].toUpperCase() + props.link.slice(1);
   const link = props.link ? '/' + props.link : '';
   const currentLocation = useLocation().pathname || '/';
-  const classlist = (props.className || '') + (currentLocation === link ? ' active' : '' )
+  const classlist = (props.className || '') + (currentLocation === link ? ' active' : '')
 
   return (
     <Link
-      className={
-        "nav-link "
-        + classlist}
+      className={"nav-link " + classlist}
       to={link}>{title}
     </Link>
   );
