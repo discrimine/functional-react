43daa071d2b1d43a7d580ca4e0a3a0cbaeffc334
discrimine <discrimine96@gmail.com>
2020-04-21 10:50:31 +0300

    minor changes

--- src/components/shared/EventCard.js
+++ src/components/shared/EventCard.js
@@ -7,21 +7,21 @@ export default function EventCard(props) {
 
   return (
     <div className="card-container col-4" style={{marginTop: "15px"}}>
-      <div className="card ">
+      <div className="card">
         <img src={event.cover} className="card-img-top" alt={event.title}/>
         <div className="card-body">
           <h5 className="card-title">{event.title}</h5>
-          <p className="card-text">{event.description.slice(0, 100) + '...'}</p>
+          <p className="card-text">{event.description.slice(0, 100) + "..."}</p>
           <div className="card-bottom container">
             <div className="row align-items-end justify-content-between">
               <div className="card-bottom-link">
-                <Link className="btn btn-link btn-sm" to={'/event/' + event.id}>More</Link>
+                <Link className="btn btn-link btn-sm" to={"/event/" + event.id}>More</Link>
               </div>
-              <div className="card-bottom-info" style={{fontSize: "0.875rem", textAlign: 'right'}}>
+              <div className="card-bottom-info" style={{fontSize: "0.875rem", textAlign: "right"}}>
                 <span>members: {event.members.length || 0}</span> <br />
                 <span>date: {eventDate}</span> <br />
-                <span>category: {event.category.title || ''}</span> <br />
-                <span>location: {event.location || ''}</span> <br />
+                <span>category: {event.category.title || ""}</span> <br />
+                <span>location: {event.location || ""}</span> <br />
               </div>
             </div>
           </div>
