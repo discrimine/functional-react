6f0dfea00c70d28705f323cc3f68c212ad5694f6
yana <yshchebyvok@gmail.com>
2020-04-21 23:28:54 +0300

    small fixed for room/rooms pages

--- src/components/room/Room.js
+++ src/components/room/Room.js
@@ -9,13 +9,21 @@ export default function Room(props) {
   const [room, setRoom] = useState(null);
   const { id } = props.match.params;
   const [loader, setLoader] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch(`${ROOMS_URL}/${id}`)
       .then((response) => response.json())
       .then((data) => {
-        setRoom(data || []);
         setTimeout(() => setLoader(false), 1000);
+        if (data.error) {
+          setError(true);
+        } else {
+          setRoom(data);
+          setError(false);
+        }
+
+
       })
       .catch((err) => {
         console.error(err);
@@ -28,25 +36,31 @@ export default function Room(props) {
       <Spinner />
       :
       <div className="container">
-        <div className="row">
+        {
+          error ?
+          <h2 className="text-center">EVENT NOT FOUND!</h2>
+          :
+          <div className="row">
           <div className="col-12">
             <h2 className="text-center mb-3 mt-3">{room.title}</h2>
             <div className="text-center mb-5" >
-            <StarRatings
-              rating={room.roomRating}
-              starRatedColor="blue"
-              numberOfStars={5}
-              name='rating'
-              starDimension="30px"
-            />
-            <Link
-              to="/signup"
-              type="button"
-              className="btn btn-primary"
-              style={{marginTop: '-20px', fontSize: '24px', float: 'right'}}
-            >
-                JOIN ROOM
-            </Link>
+              <StarRatings
+                rating={room.roomRating}
+                starRatedColor="blue"
+                numberOfStars={5}
+                name='rating'
+                starDimension="30px"
+              />
+              </div>
+            <div>
+              <Link
+                to="/signup"
+                type="button"
+                className="btn btn-primary"
+                style={{marginTop: '-75px', fontSize: '24px', float: 'right'}}
+              >
+                  JOIN ROOM
+              </Link>
             </div>
           </div>
           <div className="col-8">
@@ -60,46 +74,48 @@ export default function Room(props) {
             </p>
             <p className="mt-2">
               <b>Members({room.members.length}/{room.members_limit}): </b>
-              {room.members.map((member, index) => (
+              {room.members.length ? room.members.map((member, index) => (
                 <Link to={`/profile/${member.id}`} key={member.id}>
                   {member.nick}
                   {index + 1 === room.members.length ? '' : ', '}
                 </Link> 
-              ))}
+              )) : 'there are no members :('}
             </p>
             <p className="mt-3"><b>Description:</b> {room.description}</p>
             <div className="row">
           <div className="col-12">
           <h4 className="font-weight-bolder font-italic">Related events</h4>
           <div className="row">
-            {room.event.map(event => (
+            {room.event.length ? room.event.map(event => (
               <Link to={`/event/${event.id}`} className="event-container col-4" key={event.id}>
                   <img src={event.cover} alt="cover" className="w-100" />
                   <h5 className="mt-2">{event.title}</h5>
                   <p>{event.description}</p> 
               </Link>
-            ))}
+            )) : <div className="col-12">there are no events :(</div>}
           </div>
           </div>
         </div>
           </div>
           <div className="col-4">
             <h4 className="font-weight-bolder font-italic">Latest posts</h4>
-            {room.posts.map(post => (
+            {room.posts.length ? room.posts.map(post => (
               <div key={post.id} className="post-container">
                 <h5 style={{backgroundImage:`url(${post.cover})`}}>{post.title}</h5>
                 <p>{post.description}</p> 
               </div>
-            ))}
+            )) : <div>there are no posts :(</div>}
+
             <h4 className="font-weight-bolder font-italic">Latest feeds</h4>
-            {room.feeds.map(feed => (
+            {room.feeds.length ? room.feeds.map(feed => (
               <div key={feed.id} className="post-container">
                 <h5 style={{backgroundImage:`url(${feed.cover})`}}>{feed.title}</h5>
                 <p>{feed.description}</p> 
               </div>
-            ))}
+            )) : <div>there are no feeds :(</div>}
           </div>
         </div>
+        }
       </div>
   );
 }
