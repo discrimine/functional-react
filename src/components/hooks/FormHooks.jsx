f2c6ab532efc9cf9a87fb719ad08b1bbe4a6a700
AlexTheBest <aleksandr.savchuk0603@gmail.com>
2020-04-23 13:02:51 +0300

    implement signup component

---
+++ src/components/hooks/FormHooks.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const useCustomForm = () => {
+    const [inputs, setInputs] = useState({});
+    const handleInputChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    }
+    return {
+        handleInputChange,
+        inputs
+    };
+};
+export default useCustomForm;
--- src/components/users/Signup.jsx
+++ src/components/users/Signup.jsx
@@ -1,34 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import useCustomForm from '../hooks/FormHooks';
 
 export default function Signup() {
-    // const [options, setFilterOptions] = useState(new URLSearchParams());
-    //
-    // function handleChange(event) {
-    //     options.set(event.target.name, event.target.value);
-    //
-    //     setFilterOptions(options);
-    //     console.log(setFilterOptions);
-    // }
-    const options = {};
-    function handleSubmit(event) {
-        console.log(event.target.value());
-        debugger;
-        options.method = 'POST';
-        options.body = JSON.stringify({email: 'test1111@test.com', password: '123456'});
-        options.headers = { 'Content-Type': 'application/json' };
-        debugger;
-        fetch('http://localhost:3001/api/signup', options)
-            .then(response => {
-                console.log(response);
-                debugger;
-            })
-            .catch(res => {
-                console.log(res);
-            })
-    }
 
-  return (
-        <div className="container" style={{marginTop: "15px"}}>
+    const {inputs, handleInputChange} = useCustomForm();
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+    };
+    const handleSubmit = (event) => {
+        if (event) {
+            event.preventDefault();
+            options.body = JSON.stringify(inputs);
+            fetch('http://localhost:3001/api/signup', options)
+                .then(response => {
+                    const data = response.json();
+                    data.then(res => console.log(res));
+                });
+        }
+    };
+
+    return (
+        <div className="container mt-3">
             <div className="row justify-content-center">
                 <div className="col-6 align-self-center">
                     <form className="register-form" onSubmit={handleSubmit}>
@@ -43,9 +36,11 @@ export default function Signup() {
                                     type="email"
                                     className="form-control"
                                     id="inputEmail1"
+                                    name="email"
                                     aria-describedby="emailHelp"
                                     placeholder="Enter email"
-                                    // onChange={handleChange}
+                                    onChange={handleInputChange}
+                                    value={inputs.email}
                                     required/>
                                 <div className="invalid-feedback">
                                     Please provide a valid email.
@@ -60,17 +55,39 @@ export default function Signup() {
                                     className="form-control"
                                     id="inputPassword"
                                     placeholder="Password"
-                                    // onChange={handleChange}
+                                    name="password"
+                                    onChange={handleInputChange}
+                                    value={inputs.password}
                                     required/>
                                 <div className="invalid-feedback">
                                     Please provide a valid Password.
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <div className="form-group row">
+                            <label htmlFor="inputConfirmPassword" className="col-4 col-form-label">Confirm
+                                password</label>
+                            <div className="col-8">
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="inputConfirmPassword"
+                                    placeholder="Confirm password"
+                                    name="confpass"
+                                    onChange={handleInputChange}
+                                    value={inputs.confpass}
+                                    required/>
+                                <div className="invalid-feedback">
+                                    Please provide a valid Password.
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" disabled={inputs.password !== inputs.confpass}
+                                className="btn btn-primary">Submit
+                        </button>
                     </form>
                 </div>
             </div>
-    </div>
-  );
+        </div>
+    );
 }
