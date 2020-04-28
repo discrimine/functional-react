7102e992677e82c73709a31ddf7d8004e2b3d709
AlexTheBest <aleksandr.savchuk0603@gmail.com>
2020-04-28 10:50:29 +0300

    added handle error

--- src/components/users/Signup.jsx
+++ src/components/users/Signup.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import {SIGNUP_URL} from '../../constants/urls';
+import React, { useState } from 'react';
+import { SIGNUP_URL } from '../../constants/urls';
 import useCustomForm from '../hooks/FormHooks';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup(props) {
   const history = useHistory();
   const {inputs, handleInputChange} = useCustomForm();
+  const [errorMessage, setErrorMessage] = useState('');
   const options = {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
   };
+
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
@@ -17,21 +19,33 @@ export default function Signup(props) {
       fetch(SIGNUP_URL, options)
         .then(response => {
           const data = response.json();
-          data.then(user => {
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            props.setIsLoggedIn(true);
-            history.push('/profile')
+          data.then(res => {
+            if(res.token){
+              localStorage.setItem('currentUser', JSON.stringify(res));
+              props.setIsLoggedIn(true);
+              history.push('/profile')
+            } else {
+              setErrorMessage(res.error.errors.message[0]);
+            }
           });
-
-        });
+        })
+        .catch(error => {
+          console.error(error);
+        })
     }
   };
 
+  function ErrorBlock(props) {
+    return <div className="alert alert-danger mt-3" role="alert">
+      {props.error}
+    </div>
+  }
 
   return (
     <div className="container mt-3">
       <div className="row justify-content-center">
         <div className="col-6 align-self-center">
+          <h1 className="text-center">Sign Up</h1>
           <form className="register-form" onSubmit={handleSubmit}>
             <div className="form-group row">
               <label
@@ -49,6 +63,7 @@ export default function Signup(props) {
                   onChange={handleInputChange}
                   value={inputs.email || ''}
                   required/>
+                { errorMessage ? <ErrorBlock error={errorMessage}/> : null }
               </div>
             </div>
             <div className="form-group row">
@@ -80,9 +95,13 @@ export default function Signup(props) {
                   required/>
               </div>
             </div>
-            <button type="submit" disabled={inputs.password !== inputs.confpass}
-                    className="btn btn-primary">Submit
-            </button>
+            <div className="form-group row">
+              <div className="col text-center">
+                <button type="submit" disabled={inputs.password !== inputs.confpass}
+                        className="btn btn-primary">Sign Up
+                </button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,9 +4,9 @@ const ROOMS_URL = `${BASE_URL}/room`;
 
 const GET_EVENTS_URL = `${BASE_URL}/event/filter`;
 
-export const LOGIN_URL = `${BASE_URL}/signin`;
-export const SIGNUP_URL = `${BASE_URL}/signup`;
-export const PROFILE_URL = `${BASE_URL}/profile`;
+const LOGIN_URL = `${BASE_URL}/signin`;
+const SIGNUP_URL = `${BASE_URL}/signup`;
+const PROFILE_URL = `${BASE_URL}/profile`;
 
 export {
   BASE_URL,
